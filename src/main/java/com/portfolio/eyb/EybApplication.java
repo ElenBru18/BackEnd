0cf42af508f7589ccff64e003bd03595c611ca6b
ElenBru18 <elenbruzzese@gmail.com>
2022-10-27 15:32:06 -0300

    Commin en Heroku

---
+++ src/main/java/com/portfolio/eyb/EybApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.eyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EybApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EybApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/portfolio/eyb/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.eyb.Repository;
+
+import com.portfolio.eyb.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long> {
+    
+}
