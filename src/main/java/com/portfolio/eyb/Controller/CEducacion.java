7a9cb076024578b2ffb9dd84e484d1a132a1d9cd
ElenBru18 <elenbruzzese@gmail.com>
2022-10-30 05:47:23 -0300

    Cors en login

--- src/main/java/com/portfolio/eyb/Controller/CEducacion.java
+++ src/main/java/com/portfolio/eyb/Controller/CEducacion.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/educacion")
 @CrossOrigin(origins = {"http://localhost:4200","https://frontendeyb.web.app"})
 
 public class CEducacion {
